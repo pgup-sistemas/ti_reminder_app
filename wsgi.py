bc4cdbf8436f5c5108cd9610fbe069947bc9f2ab
pgup-sistemas <pageupsistemas@gmail.com>
2025-10-23 08:37:43 -0400

    corrige wsgi para usar create_app

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
-from app import app  # aqui 'app' é sua instância Flask
+from app import create_app
+
+app = create_app()  # cria a instância do Flask a partir da factory
 
 if __name__ == "__main__":
     app.run()
+
 
